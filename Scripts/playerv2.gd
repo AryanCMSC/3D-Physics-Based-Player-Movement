5dd3de62b1261b3b43cbd4d0c351fb4bee4f7527
Aryan <aryanrathi412@gmail.com>
2025-06-13 02:03:17 -0400

    fov, stairs, tweaks
    added walking up and down stairs (still kinda choppy) added fov shifting based on velocity. tweaked a few things to get the feel right.

--- Scripts/playerv2.gd
+++ Scripts/playerv2.gd
@@ -8,6 +8,7 @@ extends CharacterBody3D
 @onready var crouch_ray: RayCast3D = $crouch_ray
 @onready var camera_3d: Camera3D = $Head/head_bobber/Camera3D
 @onready var head_bobber: Node3D = $Head/head_bobber
+@onready var player_v2: CharacterBody3D = $"."
 
 # speed and movement variables
 
@@ -16,22 +17,35 @@ const walking_speed := 5.0
 const sprinting_speed := 10.0
 const crouching_speed := 3.0
 const JUMP_VELOCITY := 5
-const mouse_sens := 0.25
-var lerp_speed := 10.0
-var air_lerp_speed := 3.0
+const dashing_speed := 30
+@export var mouse_sens := 0.25
+const lerp_speed := 10.0
+const air_lerp_speed := 3.0
+const fov_lerp_speed := 8.0
 var direction := Vector3.ZERO
-var jumps := 2
+@export var jumps := 2
+@export var FOV := 70
+const FOV_CHANGE := 2
 # player states
 
 var walking := false
 var sprinting := false
 var crouching := false
 var sliding := false
+var dashing := false
+var can_dash := true
+
+# stairs
+
+const MAX_STEP_HEIGHT := 0.5
+var _snapped_to_stairs_last_frame := false
+var _last_frame_was_on_floor := -INF
+
 
 # sliding variables
 
 var slide_timer := 0.0
-var slider_time_max := 1
+var slider_time_max := 1.2
 var slide_vector := Vector2.ZERO
 var slide_speed := 13.0
 
@@ -66,6 +80,58 @@ func _input(event: InputEvent) -> void:
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
+	# walking on stairs
+
+func is_surface_too_steep(normal : Vector3) -> bool:
+	return normal.angle_to(Vector3.UP) > player_v2.floor_max_angle
+	
+func _snap_down_to_stairs_check() -> void:
+	var did_snap := false
+	var floor_below : bool = %stairs_below_ray.is_colliding() and not is_surface_too_steep(%stairs_below_ray.get_collision_normal())
+	var was_on_floor_last_frame := Engine.get_physics_frames() - _last_frame_was_on_floor == 1
+	if not is_on_floor() and velocity.y <= 0 and (was_on_floor_last_frame or _snapped_to_stairs_last_frame) and floor_below:
+		var body_test_result := PhysicsTestMotionResult3D.new()
+		if _run_body_test_motion(player_v2.global_transform, Vector3(0, -MAX_STEP_HEIGHT, 0), body_test_result):
+			var translate_y := body_test_result.get_travel().y
+			player_v2.position.y += translate_y
+			apply_floor_snap()
+			did_snap = true
+	_snapped_to_stairs_last_frame = did_snap
+	
+func _snap_up_stairs_check(delta: float) -> bool:
+	if not is_on_floor() and not _snapped_to_stairs_last_frame: return false
+	# Don't snap stairs if trying to jump, also no need to check for stairs ahead if not moving
+	if player_v2.velocity.y > 0 or (player_v2.velocity * Vector3(1,0,1)).length() == 0: return false
+	var expected_move_motion : Vector3 = player_v2.velocity * Vector3(1,0,1) * delta
+	var step_pos_with_clearance := player_v2.global_transform.translated(expected_move_motion + Vector3(0, MAX_STEP_HEIGHT * 2, 0))
+	# Run a body_test_motion slightly above the pos we expect to move to, towards the floor.
+	#  We give some clearance above to ensure there's ample room for the player.
+	#  If it hits a step <= MAX_STEP_HEIGHT, we can teleport the player on top of the step
+	#  along with their intended motion forward.
+	var down_check_result := KinematicCollision3D.new()
+	if (player_v2.test_move(step_pos_with_clearance, Vector3(0,-MAX_STEP_HEIGHT*2,0), down_check_result)
+	and (down_check_result.get_collider().is_class("StaticBody3D") or down_check_result.get_collider().is_class("CSGShape3D"))):
+		var step_height := ((step_pos_with_clearance.origin + down_check_result.get_travel()) - player_v2.global_position).y
+		# Note I put the step_height <= 0.01 in just because I noticed it prevented some physics glitchiness
+		# 0.02 was found with trial and error. Too much and sometimes get stuck on a stair. Too little and can jitter if running into a ceiling.
+		# The normal character controller (both jolt & default) seems to be able to handled steps up of 0.1 anyway
+		if step_height > MAX_STEP_HEIGHT or step_height <= 0.01 or (down_check_result.get_position() - player_v2.global_position).y > MAX_STEP_HEIGHT: return false
+		%stairs_ahead_ray.global_position = down_check_result.get_position() + Vector3(0,MAX_STEP_HEIGHT,0) + expected_move_motion.normalized() * 0.1
+		%stairs_ahead_ray.force_raycast_update()
+		if %stairs_ahead_ray.is_colliding() and not is_surface_too_steep(%stairs_ahead_ray.get_collision_normal()):
+			player_v2.global_position = step_pos_with_clearance.origin + down_check_result.get_travel()
+			apply_floor_snap()
+			_snapped_to_stairs_last_frame = true
+			return true
+	return false
+
+func _run_body_test_motion(from : Transform3D, motion : Vector3,  result : PhysicsTestMotionResult3D = null) -> bool:
+		if not result : result = PhysicsTestMotionResult3D.new()
+		var params := PhysicsTestMotionParameters3D.new()
+		params.from = from
+		params.motion = motion
+		return PhysicsServer3D.body_test_motion(player_v2.get_rid(), params, result)
+		
 func _physics_process(delta: float) -> void:
 	
 	# movement input to use for sliding checking and for direction.
@@ -76,7 +142,26 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	else:
 		jumps = 2
-
+		_last_frame_was_on_floor = Engine.get_physics_frames()
+	
+	# dashing 
+		
+	if Input.is_action_just_pressed("dash") and can_dash:
+		
+		dashing = true
+		can_dash = false
+		
+		var dash_duration := 0.2
+		await get_tree().create_timer(dash_duration).timeout
+		
+		dashing = false
+		
+		var dash_cooldown := 2.0
+		
+		await get_tree().create_timer(dash_cooldown).timeout
+		
+		can_dash = true
+	
 	# Jump
 	if Input.is_action_just_pressed("jump") and jumps == 2:
 		velocity.y = JUMP_VELOCITY
@@ -86,12 +171,7 @@ func _physics_process(delta: float) -> void:
 		jumps -= 1
 		velocity.y = JUMP_VELOCITY * 1.2
 		sliding = false
-	#if Input.is_action_just_pressed("jump") and jumps == 2:
-		#jumps -= 1
-		#velocity.y = JUMP_VELOCITY
-	#elif Input.is_action_just_pressed("jump") and jumps == 1:
-		#jumps -= 1
-		
+	
 	# Crouching and Sprinting (note crouching is dominant over sprinting)
 	if Input.is_action_pressed("crouch") || sliding:
 		
@@ -102,7 +182,7 @@ func _physics_process(delta: float) -> void:
 		crouching_collision.disabled = false
 		
 		# start sliding logic if sprinting in a direction while crouching
-		if sprinting and input_dir != Vector2.ZERO:
+		if sprinting and input_dir != Vector2.ZERO and is_on_floor():
 			sliding = true
 			slide_vector = input_dir
 			slide_timer = slider_time_max
@@ -152,7 +232,7 @@ func _physics_process(delta: float) -> void:
 		head_bobbing_index += head_bobbing_crouching_speed * delta
 		
 	# headbob checking and logic
-	if is_on_floor() && !sliding && input_dir != Vector2.ZERO:
+	if (is_on_floor() or _snapped_to_stairs_last_frame) && !sliding && input_dir != Vector2.ZERO:
 		head_bobbing_vector.y = sin(head_bobbing_index)
 		head_bobbing_vector.x = sin(head_bobbing_index/2)*0.5
 		
@@ -162,9 +242,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		head_bobber.position.y = lerp(head_bobber.position.y, 0.0, delta * lerp_speed)
 		head_bobber.position.x = lerp(head_bobber.position.x, 0.0, delta * lerp_speed)
+		
+	# fov
+	
+	var velocity_clamped : float = clamp(velocity.length(), walking_speed, dashing_speed * 4)
+	var target_fov := FOV + FOV_CHANGE * velocity_clamped
+	camera_3d.fov = lerp(camera_3d.fov, target_fov, fov_lerp_speed * delta)
+	
 	
 	# Directional Movement
-	if is_on_floor():
+	if is_on_floor() or _snapped_to_stairs_last_frame:
 		direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), lerp_speed * delta)
 	else:
 		if input_dir != Vector2.ZERO:
@@ -174,6 +261,11 @@ func _physics_process(delta: float) -> void:
 		direction = (transform.basis * Vector3(slide_vector.x,0, slide_vector.y)).normalized()
 		current_speed = (slide_timer + 0.1) * slide_speed
 		
+	if dashing:
+		direction = -player_v2.transform.basis.z.normalized()
+		current_speed = dashing_speed
+		velocity = current_speed * direction
+		
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
@@ -181,5 +273,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
-
-	move_and_slide()
+		
+	if not _snap_up_stairs_check(delta):
+		move_and_slide()
+		_snap_down_to_stairs_check()
+		
